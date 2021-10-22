2ddcf54de3ad574f92aff481fe9612fa8fa8bee2
l3gacyb3ta <l3gacy.b3ta@disroot.org>
2021-10-22 14:47:04 -0400

    Left sidebar

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,63 @@
-import Head from 'next/head'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div className={styles.background + " h-screen"}>
+      <div className="overflow-hidden">
+        <Head>
+          <title>Create Next App</title>
+          <link rel="icon" href="/favicon.ico" />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Space+Grotesk"
+          />
+        </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+        <main className="">
+          <img
+            className="opacity-60 absolute right-96 -top-10 z-0"
+            height="100%"
+            width="50%"
+            src="/blobs/big.svg"
+          />
+          <img
+            className="opacity-60 absolute left-0 top-0 -ml-32 -mt-32 z-0"
+            height="23%"
+            width="23%"
+            src="/blobs/little1.svg"
+          />
+          <img
+            className="opacity-60 absolute -right-48 -bottom-24"
+            height="23%"
+            width="34%"
+            src="/blobs/big2.svg"
+          />
+          <img
+            className="opacity-60 absolute -bottom-32 left-20 "
+            height="23%"
+            width="23%"
+            src="/blobs/little2.svg"
+          />
+          <div className="ml-5">
+            <div className="z-10">
+              <h1 className="text-6xl font-black m-4">Comp</h1>
+              <p className="text-gray-500 ml-5 mb-2">
+                A computing platform for the future
+              </p>
+              <img src="/lines/line.svg" styles="" className="ml-14" />
+            </div>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+            <div className="border-l-2 border-gray-500 ml-5 pl-1 text-gray-500 mb-2 absolute bottom-8">
+              <h2 className="font-bold">HOME</h2>
+              <h2 className="hover:font-bold duration-150">PEOPLE</h2>
+              <h2 className="hover:font-bold duration-150">SOFTWARE</h2>
+              <h2 className="hover:font-bold duration-150">HARDWARE</h2>
+            </div>
+          </div>
+        </main>
+      </div>
     </div>
-  )
+  );
 }
--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,8 @@
 // https://tailwindcss.com/docs/using-with-preprocessors
 module.exports = {
   plugins: {
-    tailwindcss: {},
+    tailwindcss: {
+    },
     autoprefixer: {},
   },
 }
